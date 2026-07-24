552a0216201a5aba7ef53653c8ef419fe21595e3
INCOGNITO <adeleyedara4@gmail.com>
2026-07-24 11:34:42 -0700

    resolve issue

--- src/App.jsx
+++ src/App.jsx
@@ -116,7 +116,7 @@ export default function App() {
 
         {/* Setup */}
         <Route
-          path="/skillselection"
+          path="/skillSelection"
           element={
             <PrivateRoute>
               <SkillSelection />
